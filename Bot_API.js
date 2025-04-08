7d104761134d790b5cbfda9ec765d363d83dba8a
Leebot1 <sergei.xomkaGmail.ru>
2025-04-08 17:03:47 +0300

    modified:   Bot_API.js

--- Bot_API.js
+++ Bot_API.js
@@ -17,7 +17,7 @@ bot.on("callback_query", function (query) {
         bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
     } else {
         queries[query.id] = query;
-        let gameurl = "https://github.com/wtfaq/pixelbattle_2/blob/main/Bot_API.js";
+        let gameurl = "https://github.com/wtfaq/pixelbattle_2/blob/main/public/index/";
         bot.answerCallbackQuery({
             callback_query_id: query.id,
             url: gameurl
